98c4c9718db69d5d67a4ad72a6a95c04ea1139ba
Yuriy Perezhogin <wolfviking086@gmail.com>
2025-02-09 09:22:16 +0500

    add mod1

---
+++ Mod1/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 12;
+
+        // y = x * y;
+        //y = x + y;
+
+        x = y - x;
+        y = y - x;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
